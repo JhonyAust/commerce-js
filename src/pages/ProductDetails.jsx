ef9acd3d97e64687086a74af0fdcdcb370052bb8
Jhony <jhonyaustcse37@gmail.com>
2023-11-14 01:32:13 +0600

    Commerce.js api ecommerce

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -24,7 +24,7 @@ const ProductDetails = () => {
   }
 
   // destructure product
-  const { title, price, description, image } = product;
+  const { image: { url: imageUrl }, name, price,description } = product;
   return (
     <section className="pt-[450px] md:pt-32 pb-[400px] md:pb-12 lg:py-32 h-screen flex items-center">
       <div className="container mx-auto">
@@ -32,12 +32,12 @@ const ProductDetails = () => {
         <div className="flex flex-col lg:flex-row items-center">
           {/* image */}
           <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0">
-            <img className="max-w-[200px] lg:max-w-xs" src={image} alt="" />
+            <img className="max-w-[200px] lg:max-w-xs" src={imageUrl} alt="" />
           </div>
           {/* text */}
           <div className="flex-1 text-center lg:text-left">
-            <h1 className="text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0">{title}</h1>
-            <div className="text-2xl text-red-500 font-medium mb-6">$ {price}</div>
+            <h1 className="text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0">{name}</h1>
+            <div className="text-2xl text-red-500 font-medium mb-6">$ {price.raw}</div>
             <p className="mb-8">{description}</p>
             <button onClick={()=>addToCart(product, product.id)} className='bg-primary py-4 px-8 text-white'>Add to cart</button>
           </div>
